9e8e1143ae96f1434a07ccb212dd593ff103cb48
janak-07 <janakdiyora8@gmail.com>
2025-03-15 07:47:04 -0300

    first commit

--- JS file/discover.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function(){
-    $('.slider').slick({
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        autoplay: true,
-        autoplaySpeed: 500,
-        dots:true,
-    });
-})
